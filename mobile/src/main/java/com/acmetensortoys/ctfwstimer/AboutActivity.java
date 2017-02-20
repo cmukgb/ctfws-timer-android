333694a9721f67b742692a938764bf9f74b3908c
Nathaniel Wesley Filardo <nwf@cs.jhu.edu>
2017-02-20 01:41:31 -0500

    Update About activity to include licenses
    Switch to Tabbed layout.
    Tweak (C) text while here.

--- mobile/src/main/java/com/acmetensortoys/ctfwstimer/AboutActivity.java
+++ mobile/src/main/java/com/acmetensortoys/ctfwstimer/AboutActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.webkit.WebView;
+import android.widget.TabHost;
 
 public class AboutActivity extends AppCompatActivity {
 
@@ -28,7 +29,26 @@ public class AboutActivity extends AppCompatActivity {
             }
         });
 
-        final WebView wv = (WebView) findViewById(R.id.about_text);
-        wv.loadData(getResources().getString(R.string.about_text), "text/html", null);
+        {
+            final WebView wv = (WebView) findViewById(R.id.about_text);
+            wv.loadData(getResources().getString(R.string.about_text),
+                    "text/html", null);
+        }
+
+        {
+            final WebView wv = (WebView) findViewById(R.id.about_licenses);
+            wv.loadUrl("file:///android_asset/licenses.html");
+        }
+
+        TabHost th = (TabHost) findViewById(R.id.about_tab_host);
+        th.setup();
+
+        th.addTab(th.newTabSpec("TagProg")
+                .setContent(R.id.about_tab_program)
+                .setIndicator("Program"));
+
+        th.addTab(th.newTabSpec("TagLic")
+                .setContent(R.id.about_tab_lic)
+                .setIndicator("Licenses"));
     }
 }
